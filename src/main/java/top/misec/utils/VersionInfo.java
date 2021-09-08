e0ce42c0be182827c9d52a2468d48a936c178c3b
JunzhouLiu <Junzhou2016@outlook.com>
2021-09-08 16:32:55 +0800

    增加新版本检测，增加新版本信息预览。

--- src/main/java/top/misec/utils/VersionInfo.java
+++ src/main/java/top/misec/utils/VersionInfo.java
@@ -27,11 +27,12 @@ public class VersionInfo {
 
 	public static void printVersionInfo() {
 		initInfo();
+		JsonObject jsonObject = HttpUtil.doGet("https://api.github.com/repos/JunzhouLiu/BILIBILI-HELPER-PRE/releases/latest");
+
 		log.info("-----版本信息-----");
 		log.info("当前版本: " + releaseVersion);
-		log.info("版本更新内容: " + releaseInfo);
-		log.info("最后更新日期: " + releaseDate);
+		log.info("最新版本为:{}", jsonObject.get("tag_name").getAsString().replaceAll("V", ""));
+		log.info("最新版本更新内容: " + jsonObject.get("body"));
 		log.info("项目开源地址: " + projectRepo);
-		log.info("-----版本信息-----\n");
 	}
 }
