aa5b5a0709fb8a97574239d1b26d89542194ee59
graywolf <graywolf186@gmail.com>
2021-10-04 01:09:06 +0800

    微信推送图文消息的描述修补

--- src/main/java/top/misec/push/impl/WeComAppPush.java
+++ src/main/java/top/misec/push/impl/WeComAppPush.java
@@ -52,6 +52,8 @@ public class WeComAppPush extends AbstractPush {
     @Override
     protected String generatePushBody(PushMetaInfo metaInfo, String content) {
         content = content.replaceAll("\r","").replaceAll("\n\n","\n");
+
+        String Digest = getSubString(content,"-----版本信息","版本信息-----") + getSubString(content,"\n\n本日任务","天\n");
         WeComMessageSendRequest request = new WeComMessageSendRequest();
         request.setToUser(metaInfo.getToUser());
         request.setAgentId(metaInfo.getAgentId());
@@ -65,7 +67,7 @@ public class WeComAppPush extends AbstractPush {
             WeComMessageSendRequest.Articles Articles = new WeComMessageSendRequest.Articles();
             Articles.setAuthor("小破站助手");
             Articles.setTitle("BILIBILI-HELPER任务简报");
-            Articles.setDigest(content);
+            Articles.setDigest(Digest);
             Articles.setContent(content.replaceAll("\n","<br>"));
             Articles.setThumb_media_id(metaInfo.getMediaid());
             WeComMessageSendRequest.Mpnews Mpnews = new WeComMessageSendRequest.Mpnews();
@@ -73,6 +75,35 @@ public class WeComAppPush extends AbstractPush {
             request.setMpnews(Mpnews);
         }
         return GsonUtils.toJson(request);
+    }
+
+    /**
+     * 取两个文本之间的文本值
+     * @param text 源文本 比如：欲取全文本为 12345
+     * @param left 文本前面
+     * @param right  后面文本
+     * @return 返回 String
+     */
+
+    public static String getSubString(String text, String left, String right) {
+        String result = "";
+        int zLen;
+        if (left == null || left.isEmpty()) {
+            zLen = 0;
+        } else {
+            zLen = text.indexOf(left);
+            if (zLen > -1) {
+                zLen += left.length();
+            } else {
+                return result;
+            }
+        }
+        int yLen = text.indexOf(right, zLen);
+        if (yLen < 0 || right == null || right.isEmpty()) {
+            yLen = text.length();
+        }
+        result = left + text.substring(zLen, yLen) + right;
+        return result;
     }
 
     @Override
